3510a4d023dd7fc3119ac879f0d6664ded5a33d4
Tito Ciuro <tito@webbo.com>
2022-03-30 18:48:09 -0700

    Initial Commit

---
+++ StateObjectDemo/StateObjectDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateObjectDemoApp.swift
+//  StateObjectDemo
+//
+//  Created by Tito Ciuro on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct StateObjectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
